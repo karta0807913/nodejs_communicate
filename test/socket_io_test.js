a80507a5e19680e524c8080acf313c41c8f9971c
karta0807913 <karta0807913@gmail.com>
2020-01-09 09:34:18 +0800

    fix error and add test

--- test/socket_io_test.js
+++ test/socket_io_test.js
@@ -14,12 +14,25 @@ server.listen(port, async ()=> {
     server_sender.retry_connect_time = 100;
     const server_receiver = Socket.CreateReceiver(server_adapter);
 
+    const client_adapter = new Adapter.SocketIOAdapter(client, topic);
+    const client_sender = Socket.CreateSender(client_adapter);
+    client_sender.retry_connect_time = 100;
+    const client_receiver = Socket.CreateReceiver(client_adapter);
+
     try {
-        await unit_test(new CommunicateManager(server_sender, server_receiver));
+        console.log("test client send to server");
+        await unit_test(new CommunicateManager(client_sender, server_receiver));
+        console.log("test server send to client");
+        await unit_test(new CommunicateManager(server_sender, client_receiver));
+        console.log("test finish");
     } catch(error) {
         console.log(error);
         process.exit(1);
     } finally {
+        server_sender.close();
+        server_receiver.close();
+        client_sender.close();
+        client_receiver.close();
         server.close();
     }
 
